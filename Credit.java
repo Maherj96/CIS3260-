08cfc6130efcb3f2c20cf58ff2c1bf2f857e9bb4
Maherj96 <maherj96@gmail.com>
2017-03-06 17:03:19 -0500

    Add files via upload

--- Credit.java
+++ Credit.java
@@ -1,131 +1,81 @@
-
-
 import java.util.Scanner;
+public class Credit{
+      public static void main(String[] args) {
+     Scanner in = new Scanner(System.in);
+        int count = 0;
+        long array[] = new long [16];
+       do
+       {
+        count = 0;
+       array = new long [16];
+        System.out.print("Enter your Credit Card Number : ");
+        long number = in.nextLong();
+        for (int i = 0; number != 0; i++) {
+        array[i] = number % 10;
+        number = number / 10;
+        count++;
+        }
+       }
+        while(count < 13); 
+        if ((array[count - 1] == 4) || (array[count - 1] == 5) || (array[count - 1] == 3 && array[count - 2] == 7)){
+            if (isValid(array) == true) {
+                System.out.println("\n  Valid. ");
+        } else {
+            System.out.println("\n  Invalid. ");
+        }
+        } else{
+          System.out.println("\n  Invalid. ");
+        }
+    }
 
-public class Credit {
-
-    public static boolean isValid(long number) {
-
-        int total = sumOfOddPlace(number) + sumOfOddPlace(number);
-
-
-        if ((total % 10 == 0) && (firstNumber(number, 1) == true) && (Size(number)>=13 ) && (Size(number)<=16 )) {
+    public static boolean isValid(long[] array) {
+        int total = sumOfDoubleEvenPlace(array) + sumOfOddPlace(array);        
+        if ((total % 10 == 0)) {
+         for (int i=0; i< array.length; i++){
+            System.out.println(array[i]);}
             return true;
         } else {
+          for (int i=0; i< array.length; i++){
+            }
             return false;
         }
     }
 
-    public static int Digit(int number) {
-
+    public static int getDigit(int number) {
         if (number <= 9) {
             return number;
         } else {
             int firstDigit = number % 10;
             int secondDigit = (int) (number / 10);
-
             return firstDigit + secondDigit;
         }
     }
-    public static int sumOfOddPlace(long number) {
+
+    public static int sumOfOddPlace(long[] array) {
         int result = 0;
-
-        while (number > 0) {
-            result += (int) (number % 10);
-            number = number / 100;
-        }
-
+        for (int i=0; i< array.length; i++)
+        {
+        while (array[i] > 0) {
+            result += (int) (array[i] % 10);
+            array[i] = array[i] / 100;
+         }}
+        
         return result;
     }
 
-    public static int sumOfDoubleEvenPlace(long number) {
-
+    public static int sumOfDoubleEvenPlace(long[] array) {
         int result = 0;
         long temp = 0;
-
-        while (number > 0) {
-            temp = number % 100;
-            result += Digit((int) (temp / 10) * 2);
-            number = number / 100;
+        for (int i=0; i< array.length; i++){
+        while (array[i] > 0) {
+             temp = array[i] % 100;
+             result += getDigit((int) (temp / 10) * 2);
+            array[i] = array[i] / 100;
+           }
         }
-
+        
         return result;
     }
-
-    public static boolean firstNumber(long number, int y) {
-
-        if ((getPrefix(number, y) == 4)
-                || (getPrefix(number,y) == 5)
-                || (getPrefix(number, y) == 3)) {
-
-            if (getPrefix(number, y) == 3) {
-                System.out.println("\nVisa Card ");
-            } else if (getPrefix(number, y) == 5) {
-                System.out.println("\nMaster Card ");
-            } else if (getPrefix(number, y) == 3) {
-                System.out.println("\nAmerican Express Card ");
-            }
-            else if (getPrefix(number,y) == 6){
-            	System.out.println("\n Discover Card");
-            }
-
-            return true;
-
-        } else {
-
-            return false;
-
-        }
-    }
-
-    public static int Size(long d) {
-
-        int count = 0;
-
-        while (d > 0) {
-            d = d / 10;
-
-            count++;
-        }
-
-        return count;
-
-    }
-
-    public static long getPrefix(long number, int k) {
-
-        if (Size(number) < k) {
-            return number;
-        } else {
-
-            int size = (int) Size(number);
-
-            for (int i = 0; i < (size - k); i++) {
-                number = number / 10;
-            }
-
-            return number;
-
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a credit card number: ");
-
-        long input = sc.nextLong();
-
-
-        if (isValid(input) == true) {
-            System.out.println("\n" + input + "  is Valid. ");
-        } else {
-            System.out.println("\n" + input + " is Invalid. ");
-        }
-
-    }
-
-        	}
+}
+        	
         
